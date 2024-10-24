2f75e75cbfa285688839727964a16f188f6ae35b
sofiane belabbas <sofianebelabbas93220@gmail.com>
2024-10-24 11:50:22 +0200

    style(App.jsx): update styles for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css';
+import React, { useState } from 'react';
 import MovieSearch from './components/MovieSearch';
+import MovieDetails from './components/MovieDetails';
+import styled from 'styled-components';
 
-function App() {
+const AppContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+`;
+
+const MovieContainer = styled.div`
+  width: 40%; // MovieSearch prend 40%
+  height: 100%; // Prendre toute la hauteur
+  overflow-y: auto; // Permettre le défilement
+`;
+
+const DetailsContainer = styled.div`
+  width: 55%; // MovieDetails prend 55%
+
+`;
+
+const App = () => {
+  const [selectedMovie, setSelectedMovie] = useState(null); // État pour le film sélectionné
+
+  const handleMovieSelect = (movie) => {
+    setSelectedMovie(movie); // Met à jour l'état avec le film sélectionné
+  };
+
   return (
-    <div>
-        <MovieSearch />
-    </div>
+    <AppContainer>
+      <MovieContainer>
+        <MovieSearch onMovieSelect={handleMovieSelect} /> {/* Passer la fonction de sélection */}
+      </MovieContainer>
+      <DetailsContainer>
+        <MovieDetails movie={selectedMovie} /> {/* Passer le film sélectionné */}
+      </DetailsContainer>
+    </AppContainer>
   );
-}
+};
 
 export default App;
